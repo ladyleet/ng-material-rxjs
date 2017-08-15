943e283bbd4d2007cfbeae7dd58f5c2d63ccfcb5
Tracy Lee <tracy@thisdot.co>
2017-08-15 10:20:13 -0700

    wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MdSidenavModule, MdToolbarModule, MdIconModule, MdButtonModule, MdProgressBarModule, MdCardModule, MdGridListModule } from '@angular/material'; 
 import { routing } from './app.routing';
 
 import 'hammerjs';
 
+import { OperatorsService } from './operators.service';
+import { TeamService } from './team.service';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { OperatorsComponent } from './operators/operators.component';
+import { TeamComponent } from './team/team.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    OperatorsComponent,
+    TeamComponent
   ],
   imports: [
     BrowserModule, 
-    routing
+    routing,
+    MdSidenavModule,
+    MdToolbarModule,
+    MdIconModule,
+    MdButtonModule,
+    MdProgressBarModule,
+    MdCardModule,
+    MdGridListModule
   ],
-  providers: [],
+  providers: [TeamService, OperatorsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,10 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { OperatorsComponent } from './operators/operators.component';
+import { TeamComponent } from './team/team.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent }
+  { path: 'about', component: AboutComponent },
+  { path: 'team', component: TeamComponent },
+  { path: 'operators', component: OperatorsComponent }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
