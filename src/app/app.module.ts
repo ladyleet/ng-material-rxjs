759d6b50e3f6dc4b4d0e5392c1aa2fa2ffb75be1
Tracy Lee <tracy@thisdot.co>
2017-08-15 14:24:47 -0700

    yay

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MdToolbarModule, MdIconModule, MdButtonModule, MdSidenavModule, MdGridListModule, MdListModule, MdProgressBarModule, MdCardModule } from '@angular/material';
 import { routing } from './app.routing';
 
 import 'hammerjs';
 
+import { TeamService } from './team.service';
+import { OperatorsService } from './operators.service';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { TeamComponent } from './team/team.component';
+import { OperatorsComponent } from './operators/operators.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    TeamComponent,
+    OperatorsComponent
   ],
   imports: [
     BrowserModule, 
-    routing
+    routing,
+    MdToolbarModule, 
+    MdIconModule, 
+    MdButtonModule, 
+    MdSidenavModule,
+    MdGridListModule,
+    MdListModule,
+    MdProgressBarModule,
+    MdCardModule
   ],
-  providers: [],
+  providers: [OperatorsService, TeamService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
