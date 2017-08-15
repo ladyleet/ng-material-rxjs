812fe29f7908d750b8bef7b9262cb15a6ecd7b19
Tracy Lee <tracy@thisdot.co>
2017-08-15 09:43:23 -0700

    add hammerjs to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { routing } from './app.routing';
 
+import 'hammerjs';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
