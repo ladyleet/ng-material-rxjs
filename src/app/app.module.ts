315760fd301ab6b3ace057604fbc13fe53ba1b93
Tracy Lee <tracy@thisdot.co>
2017-08-15 11:30:08 -0700

    wtf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { routing } from './app.routing';
+import { MdSidenavModule, MdToolbarModule, MdIconModule, MdButtonModule } from '@angular/material';
+
 
 import 'hammerjs';
+
+import { TeamService } from './team.service';
+import { OperatorsService } from './operators.service';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { TeamComponent } from './team/team.component';
+import { OperatorsComponent } from './operators/operators.component';
+
+import { routing } from './app.routing';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    TeamComponent,
+    OperatorsComponent
   ],
   imports: [
     BrowserModule, 
-    routing
+    routing,
+    MdSidenavModule,
+    MdToolbarModule,
+    MdIconModule,
+    MdButtonModule
   ],
-  providers: [],
+  providers: [OperatorsService, TeamService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
