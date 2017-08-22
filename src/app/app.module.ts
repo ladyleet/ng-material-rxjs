64fede3d54700fcd057a508c1788855758192be2
Tracy Lee <tracy@thisdot.co>
2017-08-22 07:37:03 -0700

    run 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { MdToolbarModule, MdIconModule, MdSidenavModule, MdButtonModule, MdProgressBarModule, MdGridListModule, MdListModule, MdCardModule, MdTooltipModule } from '@angular/material';
+
 import { routing } from './app.routing';
 
 import 'hammerjs';
 
+import { TeamService } from './team.service';
+import { OperatorsService } from './operators.service';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { OperatorsComponent } from './operators/operators.component';
+import { TeamComponent } from './team/team.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    OperatorsComponent,
+    TeamComponent
   ],
   imports: [
     BrowserModule, 
     BrowserAnimationsModule,
+    MdToolbarModule, 
+    MdIconModule, 
+    MdSidenavModule, 
+    MdButtonModule,
+    MdProgressBarModule,
+    MdGridListModule,
+    MdListModule,
+    MdCardModule,
+    MdTooltipModule,
     routing
   ],
-  providers: [],
+  providers: [TeamService, OperatorsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
